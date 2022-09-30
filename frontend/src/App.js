234c7f5527cc4fa999ad1a360d303de0becd5231
Kyle Wisecarver <wiseworks.2022@gmail.com>
2022-09-30 03:27:32 -0500

    docs: updated apps.js to match source code: https://github.com/dCC-Online/ReactDjango_JWT_StarterCode.git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+// General Imports
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Pages Imports
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
+// Component Imports
+import Navbar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
+// Util Imports
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
